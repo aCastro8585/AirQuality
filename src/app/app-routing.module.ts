a4623c07b2f810db89460b959ae781f967daf459
aCastro8585 <jcastro8585@gmail.com>
2019-09-25 10:15:04 -0500

    Incluir rutas en el app-routing.module para habilitar navegación en la aplicación. Se actualiza el componente app.component para la navegación entre los dos componentes.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CitiesMonitoringComponent } from './air-quality-tables/cities-monitoring/cities-monitoring.component';
+import { DevicesMonitoringComponent } from './air-quality-tables/devices-monitoring/devices-monitoring.component'
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/cities', pathMatch: 'full' },
+  { path: 'cities', component: CitiesMonitoringComponent },
+  { path: 'devices', component: DevicesMonitoringComponent },
+  { path: '**', redirectTo: '/cities' }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
